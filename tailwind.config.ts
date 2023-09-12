7a3eb682844f7d86874f0872245f72a256125377
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-09-12 23:06:23 +0900

    背景を指定色に

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
@@ -16,8 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
-export default config
+  plugins: [require("flowbite/plugin")],
+};
+export default config;
