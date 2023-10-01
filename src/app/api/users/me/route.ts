4eb705969717441181466b5db1017d84f86c702e
ecto0310 <48977990+ecto0310@users.noreply.github.com>
2023-10-01 12:07:03 +0000

    [fix] format

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,13 +1,13 @@
-import { getServerSession } from 'next-auth/next';
-import { NextResponse } from 'next/server';
-import { authOptions } from '../../auth/[...nextauth]/route';
-import { PrismaClient } from '@prisma/client';
+import { getServerSession } from "next-auth/next";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
-  const session = await getServerSession(authOptions)
-  
+  const session = await getServerSession(authOptions);
+
   const user = await prisma.user.findFirst({
     where: { id: session?.user.id },
   });
